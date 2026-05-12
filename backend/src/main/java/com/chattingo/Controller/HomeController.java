b83c4fe69cf5c5ad5f4f328f62cee81630787eca
Hanuman Bhalerao <bhaleraohanuman4@gmail.com>
2026-05-12 20:06:38 +0530

    Improve API root endpoint to return industry-standard JSON response

--- backend/src/main/java/com/chattingo/Controller/HomeController.java
+++ backend/src/main/java/com/chattingo/Controller/HomeController.java
@@ -1,16 +1,17 @@
 package com.chattingo.Controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.chattingo.Payload.ApiResponse;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public ResponseEntity<String> home() {
-        return new ResponseEntity<String>("Welcome", HttpStatus.OK);
+    public ResponseEntity<?> home() {
+        return ResponseEntity.ok(new ApiResponse("Welcome to Chattingo API", true));
     }
 
 }
